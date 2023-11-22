fd4956cc665080d22dbb0bee3c71dc6580afe259
byte <50130120+huckleton@users.noreply.github.com>
2023-11-22 18:23:12 -0500

    add(.eleventy.js): "get page by id" filter

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,10 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("./src/assets/");
 	eleventyConfig.addWatchTarget("./src/assets/");
 
+	eleventyConfig.addFilter("getPageById", (collection, id) => {
+		return collection.find(item => item.data.id === id);
+	})
+		
 	return {
 		dir: {
 			input: "src",
