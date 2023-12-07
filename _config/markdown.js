77b7ad135c4cb3e30b6af1cded92303af78a46b6
byte <50130120+huckleton@users.noreply.github.com>
2023-12-07 05:30:46 -0500

    refactor(_config/markdown.js): extract functions

--- _config/markdown.js
+++ _config/markdown.js
@@ -6,11 +6,13 @@ module.exports = function(eleventyConfig) {
 	const markdownEngine = markdownIt({ html: true });
 	this.markdownEngine = markdownEngine;
 
+	const renderMarkdown = content => markdownEngine.render(content);
+
 	markdownEngine.use(attrs);
 	markdownEngine.use(markdownDiv);
 	eleventyConfig.setLibrary("md", markdownEngine);
 	
-	eleventyConfig.addPairedShortcode("renderMarkdown", content => markdownEngine.render(content));
+	eleventyConfig.addPairedShortcode("renderMarkdown", renderMarkdown);
 	
 	return {
 		markdownTemplateEngine: "njk"
