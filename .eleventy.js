811c79d222754cee5e43b34ba1f370f8a4e8556e
byte <50130120+huckleton@users.noreply.github.com>
2023-12-07 05:04:20 -0500

    remove(.eleventy.js): page-related code

--- .eleventy.js
+++ .eleventy.js
@@ -3,8 +3,6 @@ const faviconPlugin = require("eleventy-favicon");
 const eleventySass = require("@grimlink/eleventy-plugin-sass");
 const sass = require("sass");
 
-const metadata = require("./src/_data/metadata.json");
-
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(faviconPlugin, { destination: "build" }); 
 	eleventyConfig.addPlugin(eleventySass, { sass, outputPath: null });
@@ -15,58 +13,11 @@ module.exports = function (eleventyConfig) {
 	loadConfigModules(eleventyConfig);
 
 	// Filters
-
-	eleventyConfig.addFilter("whatLinksHere", function (collection) {
-		const links = [];
-		const id = this.ctx.id;
-		if (!id) return;
-
-		collection.forEach(page => {
-			if (!Array.isArray(page.data.linkedPages)) return;
-			if (page.data.linkedPages.includes(id) && !(this.ctx.linkedPages.includes(page.data.id))) {
-				links.push(page);
-			}
-		});
-
-		return links;
-	});
-
-	eleventyConfig.addFilter("alphabetizePages", pages => {
-		pages.sort((a, b) => {
-			a = (a.data.pageTitle || "").toLowerCase();
-			b = (b.data.pageTitle || "").toLowerCase();
-			return a > b ? 1 : -1;
-		});
-		return pages;
-	});
-
-	eleventyConfig.addFilter("namespaced", (collection, namespace) => {
-		return collection.filter(item => item.data.id && item.data.id.startsWith(namespace + ":"));
-	});
-
-	eleventyConfig.addFilter("getPageById", (collection, id) => {
-		return collection.find(item => item.data.id === id);
-	});
-
 	eleventyConfig.addFilter("keys", dict => {
 		return Object.keys(dict);
 	});
 
 	// Shortcodes
-
-	eleventyConfig.addShortcode("linkById", function (id, selector, fallback) {
-		const ctx = this.ctx;
-		if (!ctx) return;
-		const page = ctx.collections["all"].find(item => item.data.id === id);
-		const pageName = page && page.data && (page.data.name || page.data.pageTitle);
-		let url = page && page.url;
-		if (url && url.endsWith("/")) {
-			url = url.substring(0, -1);
-		}
-
-		return `<a href="${page && url || fallback || "#"}${selector || ""}">${pageName || fallback || "URL not found"}</a>`;
-	});
-
 	eleventyConfig.addShortcode("keyValue", (key, value) => {
 		return `
 			<tr class="field-row">
@@ -77,22 +28,6 @@ module.exports = function (eleventyConfig) {
 					${value}
 				</td>
 			</tr>
-		`;
-	});
-
-	eleventyConfig.addShortcode("thumbnail", (page, thumbnailSize) => {
-		const title = page.data.name || page.data.pageTitle || "";
-
-		return `
-			<article class="thumb-item-box" ${thumbnailSize ? `style="--thumbnail-size:${thumbnailSize}px"` : ""}>
-				<a href="${page.url}">
-					<img 
-						alt="${title}" 
-						src="${metadata.assets.img}/${page.data.thumbnailUrl}" 
-						class="thumbnail"/>
-					<div class="thumb-footer">${title}</div>
-				</a>
-			</article>
 		`;
 	});
 
