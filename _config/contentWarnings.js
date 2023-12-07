c799abea82b01f31c321f0155e28a18e9deaf855
byte <50130120+huckleton@users.noreply.github.com>
2023-12-07 05:32:04 -0500

    tweak(_config/contentWarnings.js): indent

--- _config/contentWarnings.js
+++ _config/contentWarnings.js
@@ -4,14 +4,16 @@ module.exports = function (eleventyConfig) {
 	const blurred = (content, warning) => {
 		const warningType = warning === "adult" ? "Adult content" : contentWarning.siteWarnings[warning].name;
 
-		return `<div class="blur-content blur-${warning}" data-blurred="" tabindex="0">
-			<div class="blur-inner">
-				${content}
+		return `
+			<div class="blur-content blur-${warning}" data-blurred="" tabindex="0">
+				<div class="blur-inner">
+					${content}
+				</div>
+				<div class="blur-warning">
+					This content has been marked as ${warningType}. Click to show/hide.
+				</div>
 			</div>
-			<div class="blur-warning">
-				This content has been marked as ${warningType}. Click to show/hide.
-			</div>
-		</div>`;
+		`;
 	};
 
 	eleventyConfig.addPairedShortcode("blurred", blurred);
