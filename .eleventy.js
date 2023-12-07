107dd0147668c43609eb87bdd2dcf4350199d5e4
byte <50130120+huckleton@users.noreply.github.com>
2023-12-07 04:55:52 -0500

    remove(.eleventy.js): remove image links stuff

--- .eleventy.js
+++ .eleventy.js
@@ -67,33 +67,6 @@ module.exports = function (eleventyConfig) {
 		return `<a href="${page && url || fallback || "#"}${selector || ""}">${pageName || fallback || "URL not found"}</a>`;
 	});
 
-	eleventyConfig.addPairedShortcode("gallery", (content) => {
-		return `
-			<ul class="gallery" aria-role="gallery" >
-				${content}
-			</ul>
-		`;
-	});
-
-	eleventyConfig.addShortcode("lightboxImg", (src, alt, meta, thumbnailSrc) => {
-		let formattedMetadata = "";
-		if (meta && meta.constructor == Object) {
-			let metaStrings = [];
-			for (const key in meta) {
-				metaStrings.push(`data-metadata-${key}="${meta[key]}"`);
-			}
-			formattedMetadata = metaStrings.join(" ");
-		}
-
-		return `
-			<figure class="lightbox-image">
-				<a class="lightbox-link" href="${metadata.assets.img}/${src}">
-					<img src="${metadata.assets.img}/${thumbnailSrc || src}" alt="${alt}" ${formattedMetadata} />
-				</a>
-			</figure>
-		`;
-	});
-
 	eleventyConfig.addShortcode("keyValue", (key, value) => {
 		return `
 			<tr class="field-row">
